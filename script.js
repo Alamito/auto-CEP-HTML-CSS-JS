a0284dfd9a9b899d5e7220089f59a1e566cb0a40
Alamito <alamirbobroski12@gmail.com>
2022-11-01 16:48:03 -0300

    feat - add script of the auto complete cep

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-function onlynumber(evt) {
-   var theEvent = evt || window.event;
-   var key = theEvent.keyCode || theEvent.which;
-   key = String.fromCharCode(key);
-   var regex = /^[0-9.]+$/;
-   if(!regex.test(key)) {
-      theEvent.returnValue = false;
-      if(theEvent.preventDefault) theEvent.preventDefault();
-   }
+let flagCep = false;
+
+const CEP = document.querySelector('.CEP');
+const state = document.querySelector('.state');
+const city = document.querySelector('.city');
+const district = document.querySelector('.district');
+const address = document.querySelector('.address');
+
+function getCEP() {
+    setInterval(() => {
+        const cep = CEP.value + "";
+        if (cep.length === 5) {
+            CEP.value = CEP.value + "-";
+        } else if (cep.length === 9) {
+            getAndPrintData(cep);
+        } 
+    }, 10)
 }
+
+function getAndPrintData(cep) {
+    const url = `https://cdn.apicep.com/file/apicep/${cep}.json`
+    axios.get(url)
+        .then(response => {
+            const data = response.data;
+            state.value = data.state;
+            city.value = data.city;
+            district.value = data.district;
+            address.value = data.address;
+        })
+        .catch(error => console.error(error))
+} 
+
+getCEP();
